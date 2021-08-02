d9fe9061623bb018a0a7a0a16d141e4999adf18e
Akempke <akempkedev@gmail.com>
2021-08-02 22:35:47 +0100

    error alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -10,17 +10,22 @@ class NumberOfEvents extends Component {
   handleInputChanged = (event) => {
     const value = event.target.value;
     if (value < 1) {
-      return this.setState({
+     this.setState({
         errorText: 'Please choose a number between 1 and 32',
         numberOfEvents: ''
       });
     } else if (value > 32) {
-      return this.setState({
+      this.setState({
         errorText: 'Please choose a number between 1 and 32',
         numberOfEvents: ''
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: '',
       });      
     }
-    this.props.updateEvents(this.props.location, value);
+    this.props.updateEvents(null, value);
   };
 
   render() {
